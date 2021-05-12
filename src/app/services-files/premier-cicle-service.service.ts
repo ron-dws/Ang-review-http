1174848680d8eff32477bca46db9af570dbdf807
ron <wanko@digitwebsolution.com>
2021-05-12 14:48:21 -0500

    display patients names and personalities

--- src/app/services-files/premier-cicle-service.service.ts
+++ src/app/services-files/premier-cicle-service.service.ts
@@ -9,10 +9,10 @@ export class PremierCicleServiceService {
 
   getStudents() {
    return  [
-            { "id": 1, "name":"gaby", "age": 41},
-            { "id": 2, "name":"diane", "age": 33},
-            { "id": 3, "name":"iris", "age": 35},
-            { "id": 4, "name":"moi", "age": 40},
+            { "id": 1, "name":"gaby", "age": 41, "status": "single", "personality":"Introvert"},
+            { "id": 2, "name":"diane", "age": 33, "status": "married", "personality":"Protector"},
+            { "id": 3, "name":"iris", "age": 35, "status": "single", "personality":"Extrovert"},
+            { "id": 4, "name":"moi", "age": 40, "status": "single", "personality":"Altruistic"},
           ];
   }
 }
